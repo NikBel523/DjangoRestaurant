596ab43cf4fdc02a60d0ed93aa03eaa8fc5ce0e6
NikBel523 <belyakov.nkt@gmail.com>
2023-03-01 11:21:58 +0400

    feat: CreateView added for each model

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
+from .forms import IngredientCreateForm, MenuItemCreateForm, RecipeRequirementCreateForm, PurchaseCreateForm
 
 
 def home(request):
@@ -14,16 +16,40 @@ class IngredientList(ListView):
     model = Ingredient
 
 
+class IngredientCreate(CreateView):
+    model = Ingredient
+    template_name = ""
+    form_class = IngredientCreateForm
+
+
 # MenuItem views
 class MenuItemList(ListView):
     model = MenuItem
+
+
+class MenuItemCreate(CreateView):
+    model = MenuItem
+    template_name = ""
+    form_class = MenuItemCreateForm
+
 
 # RecipeRequirement views
 class RecipeRequirementList(ListView):
     model = RecipeRequirement
 
 
+class RecipeRequirementCreate(CreateView):
+    model = RecipeRequirement
+    template_name = ""
+    form_class = RecipeRequirementCreateForm
+
+
 # Purchase views
 class PurchaseList(ListView):
     model = Purchase
 
+
+class PurchaseCreate(CreateView):
+    model = Purchase
+    template_name = ""
+    form_class = PurchaseCreateForm
